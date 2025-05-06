a13da42f5b574418008f895e707264e6f333a168
Keemon Samuel Landsfeldt <ksl@mileagebook.com>
2025-05-06 18:55:43 +0200

    - Added example endpoints

---
+++ EHCase/API/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using EHCase.Model;
+using EHCase.Repository.Interfaces;
+using System.Web.Http;
+
+namespace EHCase.API
+{
+    [Route("api/[controller]")]
+    public class ProductController
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/Product/cheapest?top=5
+        [HttpGet("cheapest")]
+        public async Task<HttpResponseMessage>GetTopXCheapestProductsAsync( int top)
+        {
+            var products = await _productRepository.GetTopXCheapestProductsAsync(top);
+            return Ok(products);
+        }
+
+    }
+}
